ba83eff1b3a2e5779b271264be844a053a6bf81a
jatinbharadwaj <jitinrcks@gmail.com>
2021-01-07 05:02:21 +0530

    post routes and get requests

---
+++ routes/postRoutes.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const Post = require("../models/postModel");
+
+router.post("/", async (req, res) => {
+  const { title, createdAt, tags, html } = req.body;
+
+  // post model
+  const newPost = new Post({ title, createdAt, tags, html });
+
+  try {
+    const savedPost = await newPost.save();
+    res.json(savedPost);
+    console.log(savedPost);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+router.get("/", async (req, res) => {
+  const posts = await Post.find();
+  res.json(posts);
+});
+
+router.get("/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.json(post);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,16 +2,27 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
+// const { postRoute } = require("./routes/postRoutes");
 
 // set up express
 
 const app = express();
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 console.log("starting server...");
-app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+app.listen(PORT, () =>
+  console.log(`Server started on port: http://localhost:${PORT}`)
+);
+
+// Set up routes
+
+app.use("/posts", require("./routes/postRoutes"));
 
 // set up mongoose
 
